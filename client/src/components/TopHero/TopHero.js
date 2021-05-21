cba04b538ba473d3ee5675597888b3e6242d1b55
luz <hernandezluz18@gmail.com>
2021-05-21 18:44:19 -0400

    updating header

--- client/src/components/TopHero/TopHero.js
+++ client/src/components/TopHero/TopHero.js
@@ -6,11 +6,8 @@ const TopHero = () => (
   <section className="hero is-primary is-medium">
     <div className="hero-body">
       <div className="hero container">
-        <h1 className="title">Hello!</h1>
-        <h1 className="title"> I'm Luz,</h1>
-        <h1 className="title">a software developer.</h1>
+        <h1 className="title"> I'm Luz</h1>
         <h2 className="subtitle">
-          
         </h2>
       </div>
     </div>
