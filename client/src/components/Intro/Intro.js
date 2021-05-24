c1c54d9dbb60649db7977b08f8326ab632642d41
luz <hernandezluz18@gmail.com>
2021-05-23 20:33:59 -0400

    modified intro

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -15,15 +15,20 @@ const Intro = () => (
     <p className="intro">
       I'm a Software Developer at TIAA.
       I currently work at TIAA as a developer on the Business Rule Management System Development team that is responsible for the decision logic layer of web and mobile apps.
-      I contributed to the development and deployment efforts to migrate 30 apps from a legacy Business Rule Management System platform to using Red Hat’s open source Decision Manager Platform.
+      I contributed to the development and deployment efforts to migrate 30 apps from a legacy Business Rule Management System platform to using an open source Platform.
       In addition, for this initiative I lead scrum, created stories in Jira, contributed to the development, and collaborated on solutions with QA and product teams.
-      </p>
-      <br />
+    </p>
+    <br />
+    <p>
+      It took one biology degree, one semester of Bioinformatics in grad school, and one coding boot camp to kick off my career in tech.
+      It has not been a straight path but, with flexibility and perseverance I now have 2 years of experience as a software developer.
+      I have had the opportunity to work with both front and back end technologies in various positions ranging from internships to my present position at TIAA.
+    </p>
       <p className="intro">
       The tools I work with includes
       with{" "}
       <strong>
-        Java, Spring Boot, Red Hat Decision Manager, Gradle, Prometheus, Artifactory, Jenkins, Electric Flow, the Command Line, Visual Studio, and Git.
+        Java, Spring Boot, Gradle, Prometheus, Artifactory, Jenkins, Electric Flow, the Command Line, Visual Studio, and Git.
       </strong>
       .
     </p>
