7611e92cc6fd3b44929fe8bff215ba8dd1aec2b2
Luz Hernandez <hernandezluz18@gmail.com>
2022-03-10 10:10:45 -0500

    update to into text

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -15,8 +15,6 @@ const Intro = () => (
     <p className="intro">
       I'm a Software Developer at TIAA.
       I currently work at TIAA as a developer on the Business Rule Management System Development team that is responsible for the decision logic layer of web and mobile apps.
-      I contributed to the development and deployment efforts to migrate 30 apps from a legacy Business Rule Management System platform to using an open source Platform.
-      In addition, for this initiative I lead scrum, created stories in Jira, contributed to the development, and collaborated on solutions with QA and product teams.
     </p>
     <br />
     <p className="intro">
@@ -26,16 +24,7 @@ const Intro = () => (
     </p>
     <br />
     <p className="intro">
-      The tools I work with includes
-      with{" "}
-      <strong>
-       Java, Spring Boot, Gradle, Prometheus, Artifactory, Jenkins, Electric Flow, the Command Line, Visual Studio, and Git.
-      </strong>
-      .
-    </p>
-    <br />
-    <p className="intro">
-        Below are examples of projects I worked on during and after the coding boot camp at the University of North Carolina at Charlotte.
+        Below are examples of projects.
     </p>
   </div>
 );
