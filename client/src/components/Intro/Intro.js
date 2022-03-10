1c78eb6e004fc24a2d00e2892b0785dbd5454890
Luz Hernandez <hernandezluz18@gmail.com>
2022-03-10 10:14:34 -0500

    changes to into text

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -19,7 +19,6 @@ const Intro = () => (
     <br />
     <p className="intro">
       It took one biology degree, one semester of Bioinformatics in grad school, and one coding boot camp to kick off my career in tech.
-      It has not been a straight path but, with flexibility and perseverance I now have 2 years of experience as a software developer.
       I have had the opportunity to work with both front and back end technologies in various positions ranging from internships to my present position at TIAA.
     </p>
     <br />
