dc17845bd1ec5841e2ae0a7a24c06331b80c94d8
Luz Hernandez <hernandezluz18@gmail.com>
2022-03-11 15:57:06 -0500

    modification to get links to open in new tabs

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css";
 
 const Button = props => (
-  <a href={props.link}>
+  <a href={props.link} target="_blank">
     <img src={props.image} className="iconButton" target="_blank" alt="iconButton"/>
   </a>
 );
