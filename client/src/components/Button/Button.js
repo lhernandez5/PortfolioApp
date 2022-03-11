7ba0637593743fb4b3b22ab0a67edf25937306e3
Luz Hernandez <hernandezluz18@gmail.com>
2022-03-11 15:58:56 -0500

    modification to img tag

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -3,7 +3,7 @@ import "./Button.css";
 
 const Button = props => (
   <a href={props.link} target="_blank">
-    <img src={props.image} className="iconButton" target="_blank" alt="iconButton"/>
+    <img src={props.image} className="iconButton" alt="iconButton"/>
   </a>
 );
 
