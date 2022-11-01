81de04accf0a90a5835e3f12ed3779dd42273db5
Luz Hernandez <hernandezluz18@gmail.com>
2022-11-01 14:07:00 -0400

    modified intro text

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -13,7 +13,11 @@ const Intro = () => (
     alt="Luz Hernandez"
   />
     <p className="intro">
-      I currently work at TIAA as a developer on the Business Rule Management System Development team that is responsible for the decision logic layer of web and mobile apps.
+      I currently work at TIAA as a software developer.I work on the execution, deployment, and maintenance of business
+      rules. This system exists as middleware to provide consumers like the call center and web with a business decision
+      based on business rules with the use of REST APIs. My responsibilities for this effort have ranged from tracking
+      progress of the agile projects in Jira, to the development of the business rules using Java, Gradle, the
+      Springboot framework, to the deployment using git, jenkins, Artifactory, and electric flow.
     </p>
     <br />
   </div>
