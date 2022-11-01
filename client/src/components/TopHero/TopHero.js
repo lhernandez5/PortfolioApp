876e4be63b91f9269cbe78a079928e272a8ce303
Luz Hernandez <hernandezluz18@gmail.com>
2022-11-01 14:57:30 -0400

    modified heading text

--- client/src/components/TopHero/TopHero.js
+++ client/src/components/TopHero/TopHero.js
@@ -6,9 +6,8 @@ const TopHero = () => (
   <section className="hero is-primary is-medium">
     <div className="hero-body">
       <div className="hero container">
-        <h1 className="title"> Hi I'm Luz Hernandez!</h1>
-        <h2 className="subtitle">
-        </h2>
+        <h1 className="title"> Hey! My Name is Luz Hernandez. I am a Software Developer Based in Charlotte, NC.</h1>
+        <h2 className="subtitle"></h2>
       </div>
     </div>
   </section>
