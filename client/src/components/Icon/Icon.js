389eef80dce5bf9353deaa07b78a516281b70aac
Luz Hernandez <hernandezluz18@gmail.com>
2024-01-26 22:53:28 -0500

    formatted file

--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./Icon.css";
 
-const Icon = props => (
+const Icon = (props) => (
   <div
     className="thumbnail"
     role="img"
     aria-label="Image"
     style={{
-      backgroundImage: `url(${props.src})`
+      backgroundImage: `url(${props.src})`,
     }}
-    // ontouchstart="this.classList.toggle('hover')"
-  >
-    {/* <h5 className="card-title">{props.title}</h5> */}
-  </div>
+  ></div>
 );
 
 export default Icon;
